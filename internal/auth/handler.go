16e2fd028163ab304fe6696313debd03f950c60c
avel <djuvotnoe@gmail.com>
2025-06-26 16:52:37 +0300

    added proper api-response from user, described payload structure

--- internal/auth/handler.go
+++ internal/auth/handler.go
@@ -1,9 +1,11 @@
 package auth
 
 import (
-	"app/url-shorter/configs"
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"app/url-shorter/configs"
 )
 
 type AuthHandlerDeps struct {
@@ -26,6 +28,12 @@ func (handler *AuthHandler) Login() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(handler.Config.Auth.Secret)
 		fmt.Println("Login")
+		response := LoginResponse{
+			Token: "1234567890",
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(201)
+		json.NewEncoder(w).Encode(response)
 	}
 }
 
