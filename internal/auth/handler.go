776244d8a91686dd2106c96720cbdce188b0f4fa
avel <djuvotnoe@gmail.com>
2025-06-26 21:09:41 +0300

    added registration

--- internal/auth/handler.go
+++ internal/auth/handler.go
@@ -1,14 +1,12 @@
 package auth
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"app/url-shorter/configs"
+	"app/url-shorter/pkg/request"
 	"app/url-shorter/pkg/response"
-
-	"github.com/go-playground/validator/v10"
 )
 
 type AuthHandlerDeps struct {
@@ -29,27 +27,24 @@ func NewAuthHandler(router *http.ServeMux, deps AuthHandlerDeps) {
 
 func (handler *AuthHandler) Login() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var payload LoginRequest
-		err := json.NewDecoder(r.Body).Decode(&payload)
+		body, err := request.HandleBody[LoginRequest](&w, r)
 		if err != nil {
-			response.Json(w, err.Error(), 402)
 			return
 		}
-		validate := validator.New()
-		err = validate.Struct(payload)
-		if err != nil {
-			response.Json(w, err.Error(), 402)
-			return
-		}
-		fmt.Println(payload)
-		res := LoginResponse{
+		fmt.Println(body)
+		data := LoginResponse{
 			Token: "1234567890",
 		}
-		response.Json(w, res, 200)
+		response.Json(w, data, 200)
 	}
 }
 
 func (handler *AuthHandler) Register() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		body, err := request.HandleBody[RegisterRequest](&w, r)
+		if err != nil {
+			return
+		}
+		fmt.Println(body)
 	}
 }
