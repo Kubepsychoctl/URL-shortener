4f3db17249bfa99fc0685b9d2f092ae88cdb926f
avel <djuvotnoe@gmail.com>
2025-08-05 08:15:16 +0300

    Finished: Authorization service

--- internal/auth/handler.go
+++ internal/auth/handler.go
@@ -1,10 +1,10 @@
 package auth
 
 import (
-	"fmt"
 	"net/http"
 
 	"app/url-shorter/configs"
+	"app/url-shorter/pkg/jwt"
 	"app/url-shorter/pkg/request"
 	"app/url-shorter/pkg/response"
 )
@@ -35,9 +35,17 @@ func (handler *AuthHandler) Login() http.HandlerFunc {
 			return
 		}
 		email, err := handler.AuthService.Login(body.Email, body.Password)
-		fmt.Println(email, err)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
+		token, err := jwt.NewJwt(handler.Config.Auth.Secret).GenerateToken(email)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 		data := LoginResponse{
-			Token: "1234567890",
+			Token: token,
 		}
 		response.Json(w, data, 200)
 	}
@@ -49,6 +57,19 @@ func (handler *AuthHandler) Register() http.HandlerFunc {
 		if err != nil {
 			return
 		}
-		handler.AuthService.Register(body.Email, body.Password, body.Name)
+		email, err := handler.AuthService.Register(body.Email, body.Password, body.Name)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		token, err := jwt.NewJwt(handler.Config.Auth.Secret).GenerateToken(email)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		data := RegisterResponse{
+			Token: token,
+		}
+		response.Json(w, data, 200)
 	}
 }
