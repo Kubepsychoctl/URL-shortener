2ec876e39d5df3d84ae0876e2e2e18cfc323583b
avel <djuvotnoe@gmail.com>
2025-08-06 10:11:03 +0300

    Provided: reading from context

--- internal/link/handler.go
+++ internal/link/handler.go
@@ -1,6 +1,7 @@
 package link
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 
@@ -56,6 +57,10 @@ func (handler *LinkHandler) Create() http.HandlerFunc {
 
 func (handler *LinkHandler) Update() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		email, ok := r.Context().Value(middleware.ContextEmailKey).(string)
+		if ok {
+			fmt.Println(email)
+		}
 		body, err := request.HandleBody[LinkUpdateRequest](&w, r)
 		if err != nil {
 			return
