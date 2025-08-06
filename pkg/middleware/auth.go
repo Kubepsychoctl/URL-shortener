a38d543ac2ff711e1b8c640a7102e2cdcd7daa60
avel <djuvotnoe@gmail.com>
2025-08-06 10:05:14 +0300

    Provided: writing to context

--- pkg/middleware/auth.go
+++ pkg/middleware/auth.go
@@ -3,18 +3,24 @@ package middleware
 import (
 	"app/url-shorter/configs"
 	"app/url-shorter/pkg/jwt"
-	"fmt"
+	"context"
 	"net/http"
 	"strings"
+)
+
+type key string
+
+const (
+	ContextEmailKey key = "ContextEmailKey"
 )
 
 func IsAuthed(next http.Handler, config *configs.Config) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		authedHeader := r.Header.Get("Authorization")
 		token := strings.TrimPrefix(authedHeader, "Bearer ")
-		isValid, data := jwt.NewJwt(config.Auth.Secret).Parse(token)
-		fmt.Println(isValid)
-		fmt.Println(data)
-		next.ServeHTTP(w, r)
+		_, data := jwt.NewJwt(config.Auth.Secret).Parse(token)
+		ctx := context.WithValue(r.Context(), ContextEmailKey, data.Email)
+		req := r.WithContext(ctx)
+		next.ServeHTTP(w, req)
 	})
 }
