176dfde5ddf8e668a4cc229697eb5f68a71cb4c5
avel <djuvotnoe@gmail.com>
2025-08-11 11:29:14 +0300

    test(auth): add E2E tests for /auth/login success and failure

--- cmd/auth_test.go
+++ cmd/auth_test.go
@@ -4,6 +4,7 @@ import (
 	"app/url-shorter/internal/auth"
 	"bytes"
 	"encoding/json"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -15,7 +16,7 @@ func TestLoginSucces(t *testing.T) {
 
 	data, _ := json.Marshal(&auth.LoginRequest{
 		Email:    "j@d.ru",
-		Password: "2",
+		Password: "1",
 	})
 
 	response, err := http.Post(server.URL+"/auth/login", "application/json", bytes.NewReader(data))
@@ -24,5 +25,36 @@ func TestLoginSucces(t *testing.T) {
 	}
 	if response.StatusCode != http.StatusOK {
 		t.Fatalf("expected %d, got %d", http.StatusOK, response.StatusCode)
+	}
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var resData auth.LoginResponse
+	err = json.Unmarshal(body, &resData)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resData.Token == "" {
+		t.Fatal("Token is nil")
+	}
+
+}
+
+func TestLoginFail(t *testing.T) {
+	server := httptest.NewServer(Init())
+	defer server.Close()
+
+	data, _ := json.Marshal(&auth.LoginRequest{
+		Email:    "j@d.ru",
+		Password: "123456",
+	})
+
+	response, err := http.Post(server.URL+"/auth/login", "application/json", bytes.NewReader(data))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if response.StatusCode != http.StatusUnauthorized {
+		t.Fatalf("expected %d, got %d", http.StatusUnauthorized, response.StatusCode)
 	}
 }
